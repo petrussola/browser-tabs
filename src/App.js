c6baad725cf251e5258ab2341c6a37699c9a7109
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-01 22:14:15 +0000

    Apply react router dom and apply style to active tab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import './App.css';
 import Tab from './Tab';
+import About from './About';
+import Features from './Features';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab>
-            <a>Home</a>
-          </Tab>
-          <Tab>
-            <a>About</a>
-          </Tab>
-          <Tab>
-            <a>Features</a>
-          </Tab>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <div className="tabs">
+            <Tab>
+              <NavLink to="/" activeClassName="is-active" exact>
+                Home{' '}
+              </NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to="/about" activeClassName="is-active">
+                About
+              </NavLink>
+            </Tab>
+            <Tab>
+              <NavLink to="/features" activeClassName="is-active">
+                Features
+              </NavLink>
+            </Tab>
+          </div>
+          <div className="viewport">
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/features">
+                <Features />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </div>
-        <div className="viewport">Pages Go Here</div>
       </div>
-    </div>
+    </Router>
   );
 }
 
